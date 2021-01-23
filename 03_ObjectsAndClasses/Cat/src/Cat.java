0139becca46d1acb76906e615b9129421e31bbf2
Dmitry Remnev <marsssvolta@gmail.com>
2021-01-23 16:24:19 +0400

    Lesson 3: Static methods and variables

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -8,17 +8,29 @@ public class Cat {
 
     private double foodEaten;
 
+    private static int count;
+
     public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
 
+        count++;
+    }
+
+    public static int getCount() {
+        return count;
     }
 
     public void pee() {
-        weight = weight - 40.0;
-        System.out.println("Сходили в туалет");
+        if (isDead()) {
+            System.out.println("Невозможно выполнить действие - кошка мертва.");
+        } else {
+            weight = weight - 40.0;
+            System.out.println("Сходили в туалет");
+            minusCat(isDead());
+        }
     }
 
     public double getFoodEaten() {
@@ -34,22 +46,51 @@ public class Cat {
     }
 
     public void meow() {
-        weight = weight - 1;
-        System.out.println("Meow");
+        if (isDead()) {
+            System.out.println("Невозможно выполнить действие - кошка мертва.");
+        } else {
+            weight = weight - 1;
+            System.out.println("Meow");
+            minusCat(isDead());
+        }
     }
 
     public void feed(Double amount) {
-        weight = weight + amount;
-        foodEaten = foodEaten + amount;
+        if (isExploded()) {
+            System.out.println("Невозможно выполнить действие - кошка взорвалась.");
+        } else {
+            weight = weight + amount;
+            foodEaten = foodEaten + amount;
+            minusCat(isExploded());
+        }
     }
 
     public void drink(Double amount) {
-        weight = weight + amount;
-        foodEaten = foodEaten + amount;
+        if (isExploded()) {
+            System.out.println("Невозможно выполнить действие - кошка взорвалась.");
+        } else {
+            weight = weight + amount;
+            foodEaten = foodEaten + amount;
+            minusCat(isExploded());
+        }
+    }
+
+    private void minusCat(boolean event) {
+        if (event) {
+            count--;
+        }
     }
 
     public Double getWeight() {
         return weight;
+    }
+
+    private boolean isDead() {
+        return weight < minWeight;
+    }
+
+    private boolean isExploded() {
+        return weight > maxWeight;
     }
 
     public String getStatus() {
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,55 +3,18 @@ public class Loader {
 
     public static void main(String[] args) {
 
-        /*// 1.Выведите в консоль вес созданных кошек.
         Cat cat = new Cat();
-        System.out.println("Вес первого кота: " + cat.getWeight());
+        cat.pee();
 
         Cat cat2 = new Cat();
-        System.out.println("Вес второго кота: " + cat2.getWeight());
+        cat2.meow();
 
         Cat cat3 = new Cat();
-        System.out.println("Вес третьего кота: " + cat3.getWeight());
+        cat3.feed(100.0);
 
         Cat cat4 = new Cat();
-        System.out.println("Вес четвёртого кота: " + cat4.getWeight());
+        cat4.drink(70.0);
 
-        Cat cat5 = new Cat();
-        System.out.println("Вес пятого кота: " + cat5.getWeight());
-        System.out.println();
-
-
-        // 2.Покормите двух кошек, после этого распечатайте их вес (убедитесь, что вес изменился).
-        System.out.println("Вес первого кота: " + cat.getWeight());
-        cat.feed(220.0);
-        cat.drink(90.0);
-        System.out.println("Вес первого кота: " + cat.getWeight());
-
-        System.out.println("Вес второго кота: " + cat2.getWeight());
-        cat2.feed(150.0);
-        cat2.drink(85.0);
-        System.out.println("Вес второго кота: " + cat2.getWeight());
-        System.out.println();
-
-        // 3.Перекормите кошку, чтобы ее статус стал Exploded.
-        while (cat.getWeight() <= cat.getMaxWeight()) {
-            cat.drink(1.0);
-        }
-        System.out.println("Статус первого кота: " + cat.getStatus());
-        System.out.println();
-
-        // 4.«Замяукайте» кошку до статуса Dead.
-        while (cat2.getWeight() >= cat2.getMinWeight()) {
-            cat2.meow();
-        }
-        System.out.println("Статус второго кота: " + cat2.getStatus());*/
-
-        Cat cat = new Cat();
-        cat.feed(150.0);
-        cat.drink(100.0);
-        cat.pee();
-        cat.pee();
-        cat.pee();
-        System.out.println("Суммарный вес съеденный кошкой: " + cat.getFoodEaten() + " граммов");
+        System.out.println(Cat.getCount());
     }
 }
