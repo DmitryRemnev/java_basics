b32113e3fabc066bfe61ec49ac2900b1437a6538
Dmitry Remnev <marsssvolta@gmail.com>
2021-01-31 11:18:15 +0400

    4.13 Homework 4.2 edits

--- 04_NumbersStringsAndDates/homework_4.2/MinAndMaxValues/src/Main.java
+++ 04_NumbersStringsAndDates/homework_4.2/MinAndMaxValues/src/Main.java
@@ -3,20 +3,27 @@ public class Main {
 
         System.out.println(Byte.MIN_VALUE);
         System.out.println(Byte.MAX_VALUE);
+        System.out.println();
 
         System.out.println(Short.MIN_VALUE);
         System.out.println(Short.MAX_VALUE);
+        System.out.println();
 
         System.out.println(Integer.MIN_VALUE);
         System.out.println(Integer.MAX_VALUE);
+        System.out.println();
 
         System.out.println(Long.MIN_VALUE);
         System.out.println(Long.MAX_VALUE);
+        System.out.println();
 
-        System.out.println(Float.MIN_VALUE);
+        System.out.println(-Float.MAX_VALUE);
+        System.out.println(-3.4028235 * (10 ^ 38));
         System.out.println(Float.MAX_VALUE);
+        System.out.println();
 
-        System.out.println(Double.MIN_VALUE);
+        System.out.println(-Double.MAX_VALUE);
+        System.out.println(-1.7976931348623157 * (10 ^ 308));
         System.out.println(Double.MAX_VALUE);
     }
 }
