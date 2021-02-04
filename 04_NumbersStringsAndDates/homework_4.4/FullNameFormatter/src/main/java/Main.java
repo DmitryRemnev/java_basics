9542f6a62ab362e361a4351f0357803e8e5eb070
Dmitry Remnev <marsssvolta@gmail.com>
2021-02-04 12:09:10 +0400

    4.21 Homework 4.4 Task number 3

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,56 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static final String VALID_CHARACTERS = " -АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+    private static int numOfString = 1;
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+    public static void main(String[] args) {
+        String surname = "";
+        String name = "";
+        String patronymic = "";
+        boolean isAddToString = false;
+        boolean isValidationSuccessful = true;
+        char c;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+            String trimString = input.trim();
+
+            for (int i = 0; i < trimString.length(); i++) {
+                c = trimString.charAt(i);
+
+                if (isAddToString && c == ' ') {
+                    isAddToString = false;
+                    numOfString++;
+                }
+
+                if (VALID_CHARACTERS.indexOf(c) != -1) {
+                    if (c != ' ') {
+                        if (numOfString == 1) {
+                            surname += c;
+                        } else if (numOfString == 2) {
+                            name += c;
+                        } else if (numOfString == 3) {
+                            patronymic += c;
+                        } else {
+                            isValidationSuccessful = false;
+                        }
+                        isAddToString = true;
+                    }
+                } else {
+                    isValidationSuccessful = false;
+                }
+            }
+
+            if (isValidationSuccessful && numOfString == 3) {
+                System.out.format("Фамилия: %s%nИмя: %s%nОтчество: %s", surname, name, patronymic);
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+            }
+        }
     }
-  }
-
 }
