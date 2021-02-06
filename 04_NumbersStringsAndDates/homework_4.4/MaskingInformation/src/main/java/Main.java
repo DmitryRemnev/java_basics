4d88e8266f70e61ba95527b1376de26120a98f37
Dmitry Remnev <marsssvolta@gmail.com>
2021-02-06 09:26:05 +0400

    4.21 Homework 4.4 Additional task

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,12 +1,32 @@
 public class Main {
+    private static Integer firstIndex;
+    private static Integer secondIndex;
 
     public static void main(String[] args) {
 
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
+        System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+        if (text.length() > 0) {
+
+            for (int i = 0; i < text.length(); i++) {
+
+                if (text.charAt(i) == '<') {
+                    firstIndex = i;
+                }
+                if (text.charAt(i) == '>') {
+                    secondIndex = i;
+                }
+            }
+
+            if ((firstIndex != null) && (secondIndex != null)) {
+                text = text.substring(0, firstIndex) + placeholder + text.substring(secondIndex + 1);
+            }
+        }
+
         return text;
     }
-
 }
