f17ba7aeaa395035e38e943b842b21648968d475
Dmitry Remnev <marsssvolta@gmail.com>
2021-02-10 11:25:37 +0400

    4.25 Homework 4.5 additional task

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+    public static final Pattern PATTERN = Pattern.compile("<.+?>");
 
     public static void main(String[] args) {
-
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
+        System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
+        Matcher matcher = PATTERN.matcher(text);
+        return matcher.replaceAll(placeholder);
     }
-
 }
