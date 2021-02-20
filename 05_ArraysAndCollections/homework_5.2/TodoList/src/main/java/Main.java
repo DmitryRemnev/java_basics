8526a73cf2baefc1288217cc04722fe9aa7073a2
Dmitry Remnev <marsssvolta@gmail.com>
2021-02-20 11:59:41 +0400

    5.7 Homework 5.2

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,80 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    private static TodoList todoList = new TodoList();
+    public static final String LIST_COMMAND = "LIST";
+    public static final String ADD_COMMAND = "ADD";
+    public static final String EDIT_COMMAND = "EDIT";
+    public static final String DELETE_COMMAND = "DELETE";
+    public static final Pattern DIGIT_PATTERN = Pattern.compile("[\\d]");
+
+    private static final TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.equals("")) {
+                break;
+            }
+
+            if (text.contains(LIST_COMMAND)) {
+
+                for (String item : todoList.getTodos()) {
+                    System.out.println(item);
+                }
+
+            } else if (text.contains(ADD_COMMAND)) {
+
+                text = text.replace(ADD_COMMAND, "").trim();
+
+                String index = getIndex(text);
+                if (index.equals("")) {
+                    todoList.add(text);
+                } else {
+                    text = text.replace(index, "").trim();
+                    todoList.add(Integer.parseInt(index), text);
+                }
+
+            } else if (text.contains(EDIT_COMMAND)) {
+
+                text = text.replace(EDIT_COMMAND, "").trim();
+
+                String index = getIndex(text);
+                if (!index.equals("")) {
+                    text = text.replace(index, "").trim();
+                    todoList.edit(text, Integer.parseInt(index));
+                }
+
+            } else if (text.contains(DELETE_COMMAND)) {
+
+                text = text.replace(DELETE_COMMAND, "").trim();
+
+                String index = getIndex(text);
+                if (!index.equals("")) {
+                    todoList.delete(Integer.parseInt(index));
+                }
+
+            } else {
+                System.out.println("Введите команду!");
+            }
+        }
+    }
+
+    public static String getIndex(String text) {
+        String index = "";
+        char c;
+
+        for (int i = 0; i < text.length(); i++) {
+            c = text.charAt(i);
+            Matcher matcher = DIGIT_PATTERN.matcher(Character.toString(c));
+            if (matcher.find()) {
+                index = index + c;
+            }
+        }
+
+        return index;
     }
 }
