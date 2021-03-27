f15aa3f5035061d1c50813678055c5f7061d6c48
Dmitry Remnev <marsssvolta@gmail.com>
2021-03-27 14:49:25 +0400

    9.8 Homework 9.2 second task

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,38 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+    public static final long TWO_HOURS_IN_MILLISECONDS = 7_200_000;
+
     public static void main(String[] args) {
 
+        Airport airport = Airport.getInstance();
 
+        for (Flight flight : findPlanesLeavingInTheNextTwoHours(airport)) {
+            System.out.println(flight.toString());
+        }
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        return airport.getTerminals()
+                .stream()
+                .flatMap((p) -> p.getFlights().stream().filter(flight -> isRightType(flight.getType()) && isRightTime(flight.getDate())))
+                .collect(Collectors.toList());
     }
 
+    public static boolean isRightType(Flight.Type type) {
+        return type.equals(Flight.Type.DEPARTURE);
+    }
+
+    public static boolean isRightTime(Date date) {
+        long flightTime = date.getTime();
+        long currentTime = System.currentTimeMillis();
+
+        return flightTime - currentTime < TWO_HOURS_IN_MILLISECONDS;
+    }
 }
