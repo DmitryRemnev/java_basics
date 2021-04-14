687a0e4a01c42024b08cb40269f4f76599935cee
Dmitry Remnev <marsssvolta@gmail.com>
2021-04-14 14:58:50 +0400

    11.5 Homework 11.1

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        Path filePath = Paths.get(path);
+
+        try {
+            return Files.walk(filePath).map(Path::toFile).filter(File::isFile).mapToLong(File::length).sum();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static String readableFileSize(long size) {
+        if (size <= 0) {
+            return "0";
+        }
+
+        final String[] units = new String[]{"Б", "КБ", "МБ", "ГБ", "ТБ"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups))
+                + " " + units[digitGroups];
     }
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    private static final String ENTER_PATH = "Введите путь до папки: ";
+    private static final String FOLDER_SIZE = "Размер папки ";
+    private static final String IS = " составляет ";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (; ; ) {
+            System.out.println(ENTER_PATH);
+
+            String path = scanner.nextLine().trim();
+            String data = FileUtils.readableFileSize(FileUtils.calculateFolderSize(path));
+
+            System.out.println(FOLDER_SIZE + path + IS + data);
+        }
     }
 }
