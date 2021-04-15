fdca5ebae0bacc12e1184e1798d1c035e2b58a45
Dmitry Remnev <marsssvolta@gmail.com>
2021-04-15 13:07:50 +0400

    11.7 Homework 11.2

--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите исходную папку:");
+        String source = scanner.nextLine().trim();
+
+        System.out.println("Введите папку назначения:");
+        String destination = scanner.nextLine().trim();
+
+        FileUtils.copyFolder(source, destination);
     }
 }
