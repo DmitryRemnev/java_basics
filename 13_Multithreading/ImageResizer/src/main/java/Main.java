490206694a9f3e0658a410c40f649b4aab0b7e31
Dmitry Remnev <marsssvolta@gmail.com>
2021-05-28 14:49:43 +0400

    13.4 Homework 13.1 edits_2

--- 13_Multithreading/ImageResizer/src/main/java/Main.java
+++ 13_Multithreading/ImageResizer/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.*;
 
 public class Main {
     public static final String SRC_FOLDER = "C:\\Users\\IGOR-K\\Desktop\\Pictures";
@@ -7,20 +8,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File srcDir = new File(SRC_FOLDER);
-
         long start = System.currentTimeMillis();
 
-        File[] files = srcDir.listFiles();
+        File srcDir = new File(SRC_FOLDER);
 
         int numberCores = Runtime.getRuntime().availableProcessors();
 
+        File[] files = srcDir.listFiles();
+
+        List<List<File>> listOfFiles = new ArrayList<>();
+        for (int i = 0; i < numberCores; i++) {
+            listOfFiles.add(new ArrayList<>());
+        }
+
         if (files != null && files.length > 0) {
 
+            for (int i = 0; i < files.length; i++) {
+                int indexBucket = i % numberCores;
+                listOfFiles.get(indexBucket).add(files[i]);
+            }
+
             for (int i = 0; i < numberCores; i++) {
-                File[] file = new File[files.length];
-                System.arraycopy(files, 0, file, 0, file.length);
-                ImageResizer imageResizer = new ImageResizer(file, NEW_WIDTH, DST_FOLDER, start);
+
+                List<File> list = listOfFiles.get(i);
+                File[] array = new File[list.size()];
+
+                for (int j = 0; j < list.size(); j++) {
+                    array[j] = list.get(j);
+                }
+
+                ImageResizer imageResizer = new ImageResizer(array, NEW_WIDTH, DST_FOLDER, start);
                 imageResizer.start();
             }
         }
