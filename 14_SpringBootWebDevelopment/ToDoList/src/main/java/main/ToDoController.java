8fd8658e473e4d565e4ddfd1a79f6da43c82ac51
Dmitry <marsssvolta@gmail.com>
2021-06-13 22:40:54 +0400

    14.14 Homework 14.3 edits

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoController.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoController.java
@@ -1,47 +1,64 @@
 package main;
 
 import main.model.ToDo;
+import main.model.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ToDoController {
 
+    @Autowired
+    private ToDoRepository toDoRepository;
+
     @GetMapping("/todo/")
     public List<ToDo> list() {
-        return Storage.getAllToDo();
+        Iterable<ToDo> toDoIterable = toDoRepository.findAll();
+
+        ArrayList<ToDo> list = new ArrayList<>();
+        for (ToDo toDo : toDoIterable) {
+            list.add(toDo);
+        }
+
+        return list;
     }
 
     @PostMapping("/todo/")
     public Long add(ToDo toDo) {
-        return Storage.addToDo(toDo);
+        ToDo newToDo = toDoRepository.save(toDo);
+        return newToDo.getId();
     }
 
     @DeleteMapping("/todo/{id}")
     public void delete(@PathVariable long id) {
-        Storage.deleteToDo(id);
+        toDoRepository.deleteById(id);
     }
 
     @PutMapping("/todo/{id}")
     public void update(@PathVariable long id, ToDo toDo) {
-        Storage.updateToDo(id, toDo);
+        toDoRepository.save(toDo);
     }
 
     @DeleteMapping("/todo/")
     public void deleteAll() {
-        Storage.deleteAllToDo();
+        toDoRepository.deleteAll();
     }
 
     @GetMapping("/todo/{id}")
     public ResponseEntity get(@PathVariable long id) {
-        ToDo toDo = Storage.getToDo(id);
-        if (toDo == null) {
+
+        Optional<ToDo> toDoOptional = toDoRepository.findById(id);
+
+        if (!toDoOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
-        return new ResponseEntity(toDo, HttpStatus.OK);
+        return new ResponseEntity(toDoOptional.get(), HttpStatus.OK);
     }
 }
