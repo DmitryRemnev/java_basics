7eaed33ad885f2d91e9ce10d6d632b00bfb81bd9
Dmitry <marsssvolta@gmail.com>
2021-06-30 09:36:57 +0400

    16.7 Homework 16.1 edits

--- 16_Performance/CarNumberGenerator/src/main/java/Loader.java
+++ 16_Performance/CarNumberGenerator/src/main/java/Loader.java
@@ -1,13 +1,18 @@
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Loader {
+    public static final String PATH_TO_FILE = "C:\\Users\\Dmitry\\IdeaProjects\\java_basics\\16_Performance\\CarNumberGenerator\\src\\main\\resources\\numbers.txt";
 
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
 
-        ParallelWriter parallelWriter = new ParallelWriter();
-        ForkJoinPool.commonPool().invoke(parallelWriter);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-        System.out.println((System.currentTimeMillis() - start) + " ms");
+        for (int i = 1; i <= Runtime.getRuntime().availableProcessors(); i++) {
+            ParallelWriter task = new ParallelWriter("Task" + i, PATH_TO_FILE + i);
+            executor.execute(task);
+
+        }
+        executor.shutdown();
     }
 }
--- 16_Performance/CarNumberGenerator/src/main/java/ParallelWriter.java
+++ 16_Performance/CarNumberGenerator/src/main/java/ParallelWriter.java
@@ -1,54 +1,42 @@
 import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.RecursiveAction;
 
-public class ParallelWriter extends RecursiveAction {
-    public static final String PATH_TO_FILE = "C:\\Users\\Dmitry\\IdeaProjects\\java_basics\\16_Performance\\CarNumberGenerator\\src\\main\\resources\\numbers.txt";
-    int numberCores = Runtime.getRuntime().availableProcessors();
+public class ParallelWriter implements Runnable {
+    private String name;
+    private String path;
+
+    public ParallelWriter(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
 
     @Override
-    protected void compute() {
-        List<ParallelWriter> taskList = new ArrayList<>();
+    public void run() {
+        long start = System.currentTimeMillis();
+        try (FileOutputStream writer = new FileOutputStream(path)) {
 
-        for (int i = 0; i < numberCores; i++) {
-            ParallelWriter task = new ParallelWriter();
-            task.fork();
-            taskList.add(task);
-        }
-
-        for (ParallelWriter task : taskList) {
-            try (FileOutputStream writer = new FileOutputStream(PATH_TO_FILE)) {
-
-                char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-                StringBuilder carNumber;
-                for (int number = 1; number < 1000; number++) {
-                    int regionCode = 199;
-                    for (char firstLetter : letters) {
-                        for (char secondLetter : letters) {
-                            for (char thirdLetter : letters) {
-                                carNumber = new StringBuilder();
-                                carNumber.append(firstLetter)
-                                        .append(padNumber(number, 3))
-                                        .append(secondLetter).append(thirdLetter)
-                                        .append(padNumber(regionCode, 2));
-                                writer.write(carNumber.toString().getBytes());
-                                writer.write('\n');
-                            }
+            char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+            StringBuilder carNumber;
+            for (int number = 1; number < 1000; number++) {
+                int regionCode = 199;
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            carNumber = new StringBuilder();
+                            carNumber.append(firstLetter)
+                                    .append(padNumber(number, 3))
+                                    .append(secondLetter).append(thirdLetter)
+                                    .append(padNumber(regionCode, 2));
+                            writer.write(carNumber.toString().getBytes());
+                            writer.write('\n');
                         }
                     }
                 }
-
-                writer.flush();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
 
-        if (taskList.size() > 0) {
-            for (ParallelWriter task : taskList) {
-                task.join();
-            }
+            writer.flush();
+            System.out.println((System.currentTimeMillis() - start) + " ms");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
